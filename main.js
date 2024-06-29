77019a9722550ac99e493fcb22faaab7366eea6e
Surya Zein Rambe <suryazein2002@gmail.com>
2024-06-29 20:02:28 +0700

    New Porto Using  HTML and Vanilla  CSS Javacript

---
+++ main.js
@@ -0,0 +1,16 @@
+var typed = new Typed(".text", {
+  strings: ["Student", "Gamers", "Fronted End", "BackEnd"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
+
+var typed = new Typed(".text2", {
+  strings: ["Student at STMIK Triguna Dharma"],
+  typeSpeed: 75,
+  // smartBackspace: true,
+  // backSpeed: 100,
+  // backDelay: 1000,
+  // loop: true,
+});
